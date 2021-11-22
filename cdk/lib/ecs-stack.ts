5520bc5fa7b37493672cc29d5ccd594c01d7ced3
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2021-11-22 02:00:00 +0000

    Add CDK and Jenkisfiles

---
+++ cdk/lib/ecs-stack.ts
@@ -0,0 +1,64 @@
+import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as elb from '@aws-cdk/aws-elasticloadbalancingv2';
+
+export interface EcsStackProps extends cdk.NestedStackProps {
+    vpc: ec2.IVpc;
+    subnets: ec2.ISubnet[];
+    albSecurityGroup: ec2.ISecurityGroup;
+    albListener: elb.IApplicationListener;
+    appId: number;
+    dnsName: string;
+}
+
+export class EcsStack extends cdk.NestedStack {
+    public fargateService: ecs.FargateService;
+    constructor(scope: cdk.Construct, id: string, props: EcsStackProps) {
+        super(scope, id, props);
+
+        const cluster = new ecs.Cluster(this, 'Cluster', { vpc: props.vpc, });
+
+        const taskDefinition = new ecs.TaskDefinition(this, 'TaskDefinition', {
+            compatibility: ecs.Compatibility.FARGATE,
+            cpu: '256',
+            memoryMiB: '512',
+        });
+
+        taskDefinition.addContainer('hujia-container', {
+            image: ecs.ContainerImage.fromRegistry('deweiliu/hujia'),
+            portMappings: [{ containerPort: 80 }],
+        });
+
+        const securityGroup = new ec2.SecurityGroup(this, 'ServiceSecurityGroup', { vpc: props.vpc, });
+
+        securityGroup.connections.allowFrom(props.albSecurityGroup, ec2.Port.tcp(80), 'Allow traffic from ELB');
+        securityGroup.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
+
+        this.fargateService = new ecs.FargateService(this, 'Service', {
+            cluster,
+            taskDefinition,
+            assignPublicIp: true,
+            vpcSubnets: { subnets: props.subnets },
+            securityGroups: [securityGroup],
+            desiredCount: 1,
+        });
+
+        const albTargetGroup = new elb.ApplicationTargetGroup(this, 'TargetGroup', {
+            port: 80,
+            protocol: elb.ApplicationProtocol.HTTP,
+            healthCheck: { enabled: true },
+            vpc: props.vpc,
+            targetType: elb.TargetType.IP,
+            targets: [this.fargateService],
+        });
+
+        new elb.ApplicationListenerRule(this, "ListenerRule", {
+            listener: props.albListener,
+            priority: props.appId * 10,
+            targetGroups: [albTargetGroup],
+            conditions: [elb.ListenerCondition.hostHeaders([props.dnsName])],
+        });
+    }
+}
+
---
+++ cdk/lib/main-stack.ts
@@ -0,0 +1,71 @@
+import * as cdk from '@aws-cdk/core';
+import * as route53 from '@aws-cdk/aws-route53';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as elb from '@aws-cdk/aws-elasticloadbalancingv2';
+import { SubnetsStack } from './subnets-stack';
+import { EcsStack } from './ecs-stack';
+import { Duration } from '@aws-cdk/core';
+
+export interface CdkStackProps extends cdk.StackProps {
+  maxAzs: number;
+  appId: number;
+  dnsRecord: string;
+  domain: string;
+}
+export class CdkStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props: CdkStackProps) {
+    super(scope, id, props);
+    const { hostedZone, igwId, vpc, alb, albSecurityGroup, albListener } = this.importValues(props);
+
+    const vpcStack = new SubnetsStack(this, 'SubnetsStack', { vpc: vpc, maxAzs: props.maxAzs, appId: props.appId, igwId });
+
+    const ecs = new EcsStack(this, 'ECS', {
+      subnets: vpcStack.subnets,
+      albSecurityGroup,
+      albListener,
+      vpc: vpc,
+      appId: props.appId,
+      dnsName: `${props.dnsRecord}.${props.domain}`,
+    });
+
+    const record = new route53.CnameRecord(this, "AliasRecord", {
+      zone: hostedZone,
+      domainName: alb.loadBalancerDnsName,
+      recordName: props.dnsRecord,
+      ttl: Duration.hours(1),
+    });
+
+    new cdk.CfnOutput(this, 'DnsName', { value: record.domainName });
+  }
+
+  importValues(props: CdkStackProps) {
+    const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
+      hostedZoneId: cdk.Fn.importValue('DLIUCOMHostedZoneID'),
+      zoneName: props.domain,
+    });
+
+    const igwId = cdk.Fn.importValue('Core-InternetGateway');
+
+    const vpc = ec2.Vpc.fromVpcAttributes(this, 'ALBVPC', {
+      vpcId: cdk.Fn.importValue('Core-Vpc'),
+      availabilityZones: cdk.Stack.of(this).availabilityZones,
+    });
+
+    const albSecurityGroup = ec2.SecurityGroup.fromSecurityGroupId(this, "ALBSecurityGroup",
+      cdk.Fn.importValue('Core-AlbSecurityGroup')
+    );
+    const albListener = elb.ApplicationListener.fromApplicationListenerAttributes(this, "ELBListener", {
+      listenerArn: cdk.Fn.importValue('Core-AlbListener'),
+      securityGroup: albSecurityGroup,
+    });
+
+    const alb = elb.ApplicationLoadBalancer.fromApplicationLoadBalancerAttributes(this, 'ALB', {
+      loadBalancerArn: cdk.Fn.importValue('Core-Alb'),
+      securityGroupId: albSecurityGroup.securityGroupId,
+      loadBalancerCanonicalHostedZoneId: cdk.Fn.importValue('Core-AlbCanonicalHostedZone'),
+      loadBalancerDnsName: cdk.Fn.importValue('Core-AlbDns'),
+    });
+
+    return { hostedZone, igwId, vpc, alb, albSecurityGroup, albListener };
+  }
+}
